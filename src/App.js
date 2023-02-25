bf6443afcd3437c0c50c441c34aa7f8f3e2f0c62
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-02-25 21:57:03 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
-
-
+import { darkTheme, lightTheme } from './utilis/Theme';
 
 const App = () => {
+   const [darkMode,setDarkMode] = useState(true)
+
   return (
+    <ThemeProvider theme={darkMode ? darkTheme:lightTheme}>
     <div className='flex'>
-      <Menu />
-      <div className='flex flex-col w-240'>
+      <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+      <div className='flex flex-col  w-[1400px] bg-gray-900'>
         <Navbar />
         <div>
-          video cards
+        <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          
+          
         </div>
       </div>
     </div>
+    </ThemeProvider>
   )
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,30 +16,30 @@ import HelpIcon from '@mui/icons-material/Help';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-const Menu = () => {
+const Menu = ({darkMode,setDarkMode}) => {
   return (
-    <div className='flex w-80 bg-black text-white h-[703px]'><div className='pb-[10px]  pl-[26px] pr-[26px] '><div className=' '><div className='flex items-center font-bold'><img className='h-30 w-20' src='https://wallpaperaccess.com/full/2404616.jpg'/>LamaTube</div>
-    <div className='mb-[15px]  border-solid border-gray-600'><div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><HomeIcon />Home</div>
+    <div className='flex w-[320px] bg-${({theme})=>theme.bg}; text-${({theme})=>theme.text}; h-[703px] sticky top-0'><div className='pb-[10px]  pl-[26px] pr-[26px] '><div className=' '><div className='flex items-center font-bold'><img className='h-30 w-20' src='https://wallpaperaccess.com/full/2404616.jpg'/>LamaTube</div>
+    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><HomeIcon />Home</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><ExploreIcon />Explore</div>
-    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><SubscriptionsIcon />Subscription</div></div>
-    <div className='mb-[15px]  border-solid border-gray-600'>
+    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><SubscriptionsIcon />Subscription</div><div className='mb-[8px]   border-${({theme})=>theme.soft}  border-2'></div>
+    <div className='mb-[15px]  border-solid border-${({theme})=>theme.soft}'>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><VideoLibraryIcon />Library</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><HistoryIcon />History</div>
-    <div className='mb-[15px] mt-4  border-solid border-gray-600'>
+    <div className='mb-[8px]   border-${({theme})=>theme.soft}  border-2'></div>
     <div className='flex flex-col pl-8 pr-4 '>Sign in to like videos, comment, and subscribe.
-      <button className='border-blue-900 text-blue-900 bg-transparent border-solid border-2 '><AccountCircleIcon />SIGN IN</button></div></div>
+      <button className='border-blue-900 text-blue-900 bg-transparent border-solid border-2 '><AccountCircleIcon />SIGN IN</button></div><div className='mb-[8px]   border-${({theme})=>theme.soft}  border-2'></div><div className='pl-6 pb-2'>BEST OF LAMATUBE</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><LibraryMusicIcon />Music</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><SportsBasketballIcon />Sports</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><SportsEsportsIcon />Gaming</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><MovieIcon />Movies</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><FeedIcon />News</div>
-    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><LiveTvIcon />Live</div></div></div>
+    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-1'><LiveTvIcon />Live</div></div><div className='mb-[8px]   border-${({theme})=>theme.soft}'></div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><SettingsIcon />Settings</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><FlagCircleIcon />Report</div>
     <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><HelpIcon />Help</div>
-    <div className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><LightModeIcon />LightMode</div>
+    <div onClick={()=>setDarkMode(!darkMode)} className='flex items-center cursor-pointer pl-6 gap-5 pb-2'><LightModeIcon />LightMode</div>
     
-    </div></div>
+    </div></div></div>
     
   )
 }
